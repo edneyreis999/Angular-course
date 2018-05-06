5df1af9d090014ded790637f65fabb5a011aec4f
Edney <edney_reis999@hotmail.com>
2018-05-06 03:11:40 -0300

    - finished first project basics

--- Project1/first-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Project1/first-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  
+  private recipes: Recipe[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    let recipe = new Recipe('Test Recipe', 'It\'s just a recipe test', 'http://br.web.img2.acsta.net/newsv7/18/04/07/02/46/3329411.jpg');
+    this.recipes.push(recipe);
   }
 
 }
--- Project1/first-project/src/app/shopping-list/shopping-list.component.ts
+++ Project1/first-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredients.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  private ingredients : Ingredient[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    let ingredient = new Ingredient('Maça', 2);
+    let ingredient2 = new Ingredient('Tomate', 5);
+
+    this.ingredients.push(ingredient);
+    this.ingredients.push(ingredient2);
   }
 
 }
